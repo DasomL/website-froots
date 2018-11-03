1c53704025fea0d2a6164d7e617e45568981bb3e
DasomL <somgrapik@gmail.com>
2018-11-03 20:14:26 +0900

    modify index-contents translate effect

---
+++ popupmenubar.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#menu").click(function() {
+    $(this).toggleClass("open");
+    $("#popup-bg").fadeToggle();
+    $("#popup-nav").toggleClass("open-popup");
+  });
+  $("#cta-btn").click(function() {
+    $(".call-to-action-popup").fadeIn();
+  });
+  $("#close").click(function() {
+    $(".call-to-action-popup").fadeOut();
+  });
+});
